4857bf700302be0eede20c89a1bd2055aeca7ab1
Rinkoff <122561323+Rinkoff@users.noreply.github.com>
2023-09-26 21:53:40 +0300

    Longest common prefix sollutin

---
+++ Python/Longest-Common-Prefix.py
@@ -0,0 +1,19 @@
+"""
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+"""
+
+def longestCommonPrefix(strs: list[str]) -> str:
+    prefix = ""
+    for i,letter in enumerate(strs[0]):
+        if all(s[i] == letter for s in strs):
+            prefix += letter
+        else:
+            break
+    return prefix
+
+strs = ["flower","flow","flight"]
+
+print(longestCommonPrefix(strs))
