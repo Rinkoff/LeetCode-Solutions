a536dd958d2ae69843fac4be0610a832c12d9665
Rinkoff <122561323+Rinkoff@users.noreply.github.com>
2023-10-19 00:40:13 +0300

    new sollution

--- Python/Remove-Duplicates-from-Sorted-Arrays.py
+++ Python/Remove-Duplicates-from-Sorted-Arrays.py
@@ -4,18 +4,29 @@ The relative order of the elements should be kept the same. Then return the numb
 Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
 """
 
+#def remove_duplicates(nums) -> int:
+    #if not nums:
+        #return 0
+
+    #unique = 0
+
+    #for i in range(1, len(nums)):
+        #if nums[i] != nums[unique]:
+            #unique += 1
+            #nums[unique] = nums[i]
+
+    #return unique + 1
+
 def remove_duplicates(nums) -> int:
-    if not nums:
-        return 0
+    if len(nums) <= 1:
+        return len(nums)
 
-    unique = 0
+    for i in range(len(nums)-1,0,-1):
+        if nums[i] == nums[i-1]:
+            nums.pop(i)
 
-    for i in range(1, len(nums)):
-        if nums[i] != nums[unique]:
-            unique += 1
-            nums[unique] = nums[i]
+    return len(nums)
 
-    return unique + 1
 
 nums = [1, 1, 2, 2, 2, 3, 4, 4, 5]
 unique_count = remove_duplicates(nums)
