ad66f805a183c675f7dbea7f33165704679b1d69
Rinkoff <122561323+Rinkoff@users.noreply.github.com>
2023-10-25 13:00:38 +0300

    Update Longest-Common-Prefix.py

--- Python/Longest-Common-Prefix.py
+++ Python/Longest-Common-Prefix.py
@@ -6,6 +6,9 @@ If there is no common prefix, return an empty string "".
 """
 
 def longestCommonPrefix(strs: list[str]) -> str:
+    if len(strs) == 0:
+        return ""
+    
     prefix = ""
     for i,letter in enumerate(strs[0]):
         if all(s[i] == letter for s in strs):
